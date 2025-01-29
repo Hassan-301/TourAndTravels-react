02ed409c7b79c4319e1ab35ec3ad7daa95b5c1af
Hassan-301 <124438591+Hassan-301@users.noreply.github.com>
2025-01-30 00:06:09 +0500

    Update Testimonials.js

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -10,7 +10,7 @@ const Testimonials = () => {
       text: `“Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, 
              there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, 
              a large language ocean.”`,
-      image: '/assets/person_1.WEBP'
+      image: '/assets/person_1.webp'
     },
     {
       id: 2,
@@ -18,7 +18,7 @@ const Testimonials = () => {
       text: `“Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, 
              there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, 
              a large language ocean.”`,
-      image: '/assets/person_2.WEBP'
+      image: '/assets/person_2.webp'
     },
     {
       id: 3,
@@ -26,7 +26,7 @@ const Testimonials = () => {
       text: `“Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, 
              there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, 
              a large language ocean.”`,
-      image: '/assets/person_3.WEBP',
+      image: '/assets/person_3.webp',
     }
   ];
 
